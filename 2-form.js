de66c406e0c431f2a5ecbc312fc9dfee6cbc9cc6
Diamond-FoxUA <dimofarb@gmail.com>
2025-09-04 20:02:06 +0000

    Deploying to gh-pages from @ Diamond-FoxUA/goit-js-hw-09@90e274171ee3712ec560a8e984cd0b8b1a9d3c37 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e={email:"",message:""},a="feedback-form-state",r=document.querySelector(".feedback-form");r.addEventListener("input",o);function o(t){t.target.name&&(e[t.target.name]=t.target.value.trim()),localStorage.setItem(a,e)}
+//# sourceMappingURL=2-form.js.map
