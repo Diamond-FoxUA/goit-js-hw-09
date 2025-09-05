c59933cf67b159a221018b868de1ebd6fbdd9421
Dmytro Farbun <dimofarb@gmail.com>
2025-09-05 23:16:12 +0300

    complete task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,14 +1,47 @@
 const formData = { email: "", message: "" };
 const keyStorage = 'feedback-form-state';
 
-const formEL = document.querySelector('.feedback-form');
+const formEl = document.querySelector('.feedback-form');
 
-formEL.addEventListener('input', inputHandler);
+loadFormData();
 
-function inputHandler(event) {
+formEl.addEventListener('input', handleInput);
+formEl.addEventListener('submit', handleSubmit)
+
+function handleInput(event) {
   if (event.target.name) {
     formData[event.target.name] = event.target.value.trim();
   }
-  localStorage.setItem(keyStorage, formData);
+  localStorage.setItem(keyStorage, JSON.stringify(formData));
 }
 
+
+function loadFormData() {
+  const savedData = localStorage.getItem(keyStorage);
+
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+
+    formEl.elements.email.value = parsedData.email || '';
+    formEl.elements.message.value = parsedData.message || '';
+
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log("Submitted data: ", formData);
+
+  localStorage.removeItem(keyStorage);
+  formData.email = "";
+  formData.message = "";
+  formEl.reset();
+}
